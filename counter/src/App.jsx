605d4e3d0b6ab81954ee465c461fd310a588039c
Anuj-java31 <anujsinghsaini31@gmail.com>
2026-01-18 22:42:16 +0530

    Project counter is created and learnt about the hooks

---
+++ counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+  let [counter, setCounter] = useState(10);
+  const addValue = () => {
+    counter = counter + 1;
+    if (counter > 20) {
+      counter = 0;
+    }
+    setCounter(counter)
+  }
+  const removeValue = () => {
+    counter = counter - 1;
+    if (counter < 0) {
+      counter = 0;
+    }
+    setCounter(counter);
+  }
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h1>Counter Value {counter}</h1>
+      <button onClick={addValue}>Add</button>
+      <br />
+      <button onClick={removeValue}>Remove</button>
+    </>
+  )
+}
+
+export default App
